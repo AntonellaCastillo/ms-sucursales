5063c3707238a2346ced6dc17209f09b6f5d3d5b
Antonella <ant.castillos@duocuc.cl>
2026-06-29 10:31:03 -0400

    feat: agregar validaciones a la clase sucursal controller

--- src/main/java/com/perfulandia/ms_sucursales/controller/SucursalController.java
+++ src/main/java/com/perfulandia/ms_sucursales/controller/SucursalController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-
+import jakarta.validation.Valid;
 
 
 
@@ -25,62 +25,47 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/api/v1/sucursales") //RUTA BASE DE LA URL A LA QUE EL CLIENTE DEBE ENVIAR LA SOLICITUD
 public class SucursalController 
 {
-    //Comunicacion con el Service
+    // Comunicacion con el Service
     @Autowired
-    private SucursalService sucursalService; 
+    private SucursalService sucursalService;
 
-    //Endpoint para guardar
+    // Endpoint para guardar
     @PostMapping
-    public Sucursal crearSucursal(@RequestBody Sucursal sucursal) 
+    public Sucursal crearSucursal(@Valid @RequestBody Sucursal sucursal) 
     {
         return sucursalService.guardarSucursal(sucursal);
     }
-    
-    //Endpoint para listar
+
+    // Endpoint para listar
     @GetMapping
     public List<Sucursal> obtenerTodas() 
     {
         return sucursalService.listarSucursal();
     }
-    
-    
-    //Endpoint para obtener. En este por id
+
+    // Endpoint para obtener por id
     @GetMapping("/{id}")
-    public ResponseEntity<Sucursal> obtenerPorId(@PathVariable Long id)
+    public ResponseEntity<Sucursal> obtenerPorId(@PathVariable Long id) 
     {
         return sucursalService.obtenerSucursalporId(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-    
 
-    //Endpoint para actualizar
+    // Endpoint para actualizar
     @PutMapping("/{id}")
-    public ResponseEntity<Sucursal> actualizar(@PathVariable Long id, @RequestBody Sucursal sucursal)
+    public ResponseEntity<Sucursal> actualizar(@PathVariable Long id, @Valid @RequestBody Sucursal sucursal) 
     {
-        try
-        {   
-            Sucursal actualizada = sucursalService.actualizarSucursal(id, sucursal);
-            return ResponseEntity.ok(actualizada);
-        
-        }   catch (RuntimeException e)
-        
-        {    
-            return ResponseEntity.notFound().build();
-        }
+        Sucursal actualizada = sucursalService.actualizarSucursal(id, sucursal);
+        return ResponseEntity.ok(actualizada);
     }
 
-    //Enpoint para eliminar
+    // Endpoint para eliminar
     @DeleteMapping("/{id}")
-
-    public ResponseEntity<Void> eliminar(@PathVariable Long id)
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) 
     {
         sucursalService.eliminarSucursal(id);
         return ResponseEntity.noContent().build();
     }
-
-
-
-}   
-
+}
 
