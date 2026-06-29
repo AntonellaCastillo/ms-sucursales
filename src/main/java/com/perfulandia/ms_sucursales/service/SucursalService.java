ab013c4c88ec4498d96466dc09f180b86fd50fba
Antonella <ant.castillos@duocuc.cl>
2026-06-28 22:30:51 -0400

    feat: agregar capa Service con CRUD de Sucursal

--- src/main/java/com/perfulandia/ms_sucursales/service/SucursalService.java
+++ src/main/java/com/perfulandia/ms_sucursales/service/SucursalService.java
@@ -1,5 +1,64 @@
 package com.perfulandia.ms_sucursales.service;
 
-public class SucursalService {
+import com.perfulandia.ms_sucursales.model.Sucursal;
+import com.perfulandia.ms_sucursales.repository.SucursalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+
+
+
+@Service
+public class SucursalService 
+{
+    @Autowired
+    private SucursalRepository sucursalRepository;
+    
+    //Métodos
+
+    //Metodo para guardar
+    //Recibe una sucursal y la guarda en la BD
+    public Sucursal guardarSucursal(Sucursal sucursal)
+    {   
+        return sucursalRepository.save(sucursal);
+    }
+
+    //Metodo para Listar sucursales
+    public List<Sucursal> listarSucursal()
+    {
+        return sucursalRepository.findAll();
+    }
+    
+    
+    //Metodo para buscar sucursales por su id 
+    public Optional<Sucursal> obtenerSucursalporId(Long id)
+    {
+        return sucursalRepository.findById(id);
+    }
+
+    public Sucursal actualizarSucursal(Long id, Sucursal sucursal)
+    {
+        Sucursal existente = sucursalRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("sucursal no existe"));
+        
+        existente.setNombre(sucursal.getNombre());
+        existente.setDireccion(sucursal.getDireccion());
+        existente.setLatitud(sucursal.getLatitud());
+        existente.setLongitud(sucursal.getLongitud());
+        existente.setHorario(sucursal.getHorario());
+
+        return sucursalRepository.save(existente);
+        
+    }   
+
+    //Metodo para eliminar una sucursal
+    public void eliminarSucursal(Long id)
+    {
+        sucursalRepository.deleteById(id);
+    }
+    
+
+    
 }
