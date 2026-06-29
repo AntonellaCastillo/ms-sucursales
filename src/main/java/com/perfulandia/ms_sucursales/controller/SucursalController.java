f57ae8b970bcc2f702dc438a25d41d67c42eeda2
Antonella <ant.castillos@duocuc.cl>
2026-06-29 00:55:28 -0400

    fix: cambiar versión del spring

--- src/main/java/com/perfulandia/ms_sucursales/controller/SucursalController.java
+++ src/main/java/com/perfulandia/ms_sucursales/controller/SucursalController.java
@@ -1,5 +1,17 @@
 package com.perfulandia.ms_sucursales.controller;
 
-public class SucursalController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.perfulandia.ms_sucursales.service.SucursalService;
 
-}
+//Marca la clase como REST: crea las APIs REST
+@RestController
+@RequestMapping("/api/v1/sucursales") //RUTA BASE DE LA URL A LA QUE EL CLIENTE DEBE ENVIAR LA SOLICITUD
+public class SucursalController 
+{
+    //Comunicacion con el Service
+    @Autowired
+    private SucursalService sucursalService; 
+}   
+
