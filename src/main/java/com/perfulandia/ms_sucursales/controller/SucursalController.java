fe946a5789195db6a986792f955e83f5330b5848
Antonella <ant.castillos@duocuc.cl>
2026-06-29 01:04:42 -0400

    feat: capa de controlador lista

--- src/main/java/com/perfulandia/ms_sucursales/controller/SucursalController.java
+++ src/main/java/com/perfulandia/ms_sucursales/controller/SucursalController.java
@@ -2,8 +2,23 @@ package com.perfulandia.ms_sucursales.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.perfulandia.ms_sucursales.model.Sucursal;
 import com.perfulandia.ms_sucursales.service.SucursalService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 //Marca la clase como REST: crea las APIs REST
 @RestController
@@ -13,5 +28,59 @@ public class SucursalController
     //Comunicacion con el Service
     @Autowired
     private SucursalService sucursalService; 
+
+    //Endpoint para guardar
+    @PostMapping
+    public Sucursal crearSucursal(@RequestBody Sucursal sucursal) 
+    {
+        return sucursalService.guardarSucursal(sucursal);
+    }
+    
+    //Endpoint para listar
+    @GetMapping
+    public List<Sucursal> obtenerTodas() 
+    {
+        return sucursalService.listarSucursal();
+    }
+    
+    
+    //Endpoint para obtener. En este por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Sucursal> obtenerPorId(@PathVariable Long id)
+    {
+        return sucursalService.obtenerSucursalporId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    
+
+    //Endpoint para actualizar
+    @PutMapping("/{id}")
+    public ResponseEntity<Sucursal> actualizar(@PathVariable Long id, @RequestBody Sucursal sucursal)
+    {
+        try
+        {   
+            Sucursal actualizada = sucursalService.actualizarSucursal(id, sucursal);
+            return ResponseEntity.ok(actualizada);
+        
+        }   catch (RuntimeException e)
+        
+        {    
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //Enpoint para eliminar
+    @DeleteMapping("/{id}")
+
+    public ResponseEntity<Void> eliminar(@PathVariable Long id)
+    {
+        sucursalService.eliminarSucursal(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 }   
+
 
